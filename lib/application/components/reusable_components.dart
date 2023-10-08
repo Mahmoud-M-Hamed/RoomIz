cb83ececa640e33ca7636f52db37fd130dc8bd13
Mahmoud Hamed <Mahmoud-M-Hamed@users.noreply.github.com>
2023-10-08 23:14:36 +0300

    reusable components and widgets

--- lib/application/components/reusable_components.dart
+++ lib/application/components/reusable_components.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:roomiz/presentation/resources/sizes_and_spaces/size_manager.dart';
 import 'package:roomiz/presentation/resources/styles/colors/colors_manager.dart';
 import 'package:roomiz/presentation/resources/styles/icons/custom_icons.dart';
@@ -6,10 +7,12 @@ import 'package:roomiz/presentation/resources/styles/text_styles/text_styles_man
 
 // -------------------------<Custom App Bar>----------------------------------
 
-PreferredSizeWidget customAppBar(
-        {required BuildContext context,
-        required String appBarTitle,
-        List<Widget>? appBarActions}) =>
+PreferredSizeWidget customAppBar({
+  required BuildContext context,
+  required VoidCallback onPressed,
+  required String appBarTitle,
+  List<Widget>? appBarActions,
+}) =>
     AppBar(
       leading: IconButton(
         icon: const Icon(
@@ -17,9 +20,7 @@ PreferredSizeWidget customAppBar(
           color: ColorsManager.kPrimaryColor,
           size: SizeManager.s30,
         ),
-        onPressed: () {
-          Navigator.of(context).pop();
-        },
+        onPressed: onPressed,
       ),
       title: Text(
         appBarTitle,
@@ -58,7 +59,62 @@ Widget customButton({
               fontColor: buttonTitleColor,
             ),
           ),
-          (buttonIcon != null) ? Icon(buttonIcon, color: ColorsManager.kWhiteColor,) : Container(),
+          (buttonIcon != null)
+              ? Icon(
+                  buttonIcon,
+                  color: ColorsManager.kWhiteColor,
+                  size: 15,
+                )
+              : Container(),
         ],
+      ),
+    );
+
+// -------------------------------<Custom TextField>------------------------------------------
+
+Widget customTextFormField({
+  TextEditingController? controller,
+  TextInputType keyboardType = TextInputType.emailAddress,
+  List<TextInputFormatter>? filteringTextInputFormatter,
+  String? Function(String?)? validation,
+  int maxLines = 1,
+  double circularBorderRadiusValue = SizeManager.s10,
+  double contentPaddingHorizontal = SizeManager.s20,
+  double contentPaddingVertical = SizeManager.s10,
+  String? labelText,
+  String? hintText,
+  bool filled = false,
+  bool readOnly = false,
+  Color fillColor = ColorsManager.kWhiteColor,
+  BorderSide borderSide = BorderSide.none,
+  Widget? prefixIcon,
+  Widget? suffixIcon,
+}) =>
+    TextFormField(
+      readOnly: readOnly,
+      keyboardType: keyboardType,
+      inputFormatters: filteringTextInputFormatter,
+      controller: controller,
+      validator: validation,
+      maxLines: maxLines,
+      cursorColor: ColorsManager.kPrimaryColor,
+      cursorHeight: 18,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsetsDirectional.symmetric(
+            horizontal: contentPaddingHorizontal,
+            vertical: contentPaddingVertical),
+        labelText: labelText,
+        hintText: hintText,
+        hintStyle: StylesManager.regularStyle(
+          fontColor: ColorsManager.kLightGrayColor,
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(circularBorderRadiusValue),
+          borderSide: borderSide,
+        ),
+        filled: filled,
+        fillColor: fillColor,
+        prefixIcon: prefixIcon,
+        suffixIcon: suffixIcon,
       ),
     );
