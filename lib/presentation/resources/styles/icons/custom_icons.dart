3ccf4fba1c74dddbb2484922efafc03754d89189
Mahmoud Hamed <Mahmoud-M-Hamed@users.noreply.github.com>
2023-10-08 23:14:15 +0300

    some custom icons

--- lib/presentation/resources/styles/icons/custom_icons.dart
+++ lib/presentation/resources/styles/icons/custom_icons.dart
@@ -7,6 +7,12 @@ class CustomIcons {
   static const String? _kFontPkg = null;
 
   static const IconData kBackArrow = Icons.chevron_left;
-  static const IconData okCircled = IconData(0xe813, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData truck = IconData(0xf0d1, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData forward =
+      IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData truck =
+      IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData okCircled =
+      IconData(0xe813, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData creditCard =
+      IconData(0xe82c, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
