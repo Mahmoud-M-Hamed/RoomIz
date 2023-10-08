e85ff45bd36e78be3f4cae2b1051280a7c1455fd
Mahmoud Hamed <Mahmoud-M-Hamed@users.noreply.github.com>
2023-10-08 23:13:41 +0300

    all fonts management

--- lib/presentation/resources/styles/fonts/fonts_manager.dart
+++ lib/presentation/resources/styles/fonts/fonts_manager.dart
@@ -1,19 +1,20 @@
-
 import 'package:flutter/material.dart';
 
-class FontWeightManager{
-
+class FontWeightManager {
   static const FontWeight kBoldFont = FontWeight.bold;
   static const FontWeight kMediumFont = FontWeight.w500;
   static const FontWeight kRegularFont = FontWeight.w400;
 }
 
-
-class FontSizeManager{
+class FontSizeManager {
   static const double s12 = 12.0;
   static const double s14 = 14.0;
+  static const double s15 = 15.0;
   static const double s16 = 16.0;
   static const double s18 = 18.0;
   static const double s22 = 22.0;
+  static const double s25 = 25.0;
+  static const double s27 = 27.0;
+  static const double s28 = 28.0;
   static const double s40 = 40.0;
 }
