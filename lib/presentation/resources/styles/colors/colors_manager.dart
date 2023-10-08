bdaddc86dd1ebaee9378074b346087134e487315
Mahmoud Hamed <Mahmoud-M-Hamed@users.noreply.github.com>
2023-10-08 23:13:50 +0300

    all colors management

--- lib/presentation/resources/styles/colors/colors_manager.dart
+++ lib/presentation/resources/styles/colors/colors_manager.dart
@@ -1,11 +1,12 @@
-
 import 'package:flutter/material.dart';
 
 class ColorsManager {
   static const Color kPrimaryColor = Color(0xff0C173C);
   static const Color kAppBackgroundColor = Color(0xffF5F4F6);
+  static const Color kWhiteW400 = Color(0xFFFFFAFA);
   static const Color kButtonColor = Color(0xff2D2942);
   static const Color kGrayColor = Color(0xff5D5F65);
+  static const Color kGrayW300Color = Color(0xffD1D1D6);
   static const Color kLightGrayColor = Color(0xff817F8E);
   static const Color kDarkYellowColor = Color(0xffFEC343);
   static const Color kWhiteColor = Colors.white;
